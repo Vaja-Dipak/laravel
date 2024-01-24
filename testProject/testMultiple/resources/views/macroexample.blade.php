a8bdb1c0d6d57de3c13fd5a1de7c67c8d617aa05
Vaja <vajadipak968@gmail.com>
2024-01-24 19:55:59 +0530

    24/01/24

---
+++ testProject/testMultiple/resources/views/macroexample.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <div class="form-group">
+            <label for="bank_name">Bank Name</label>
+            {{ Form::selectBank("bank_name", $merchant['paymentInfo']->bank_name ?? null,["class"=>"form-control"]) }}
+        </div>
+    </form>
+</body>
+
+</html>
